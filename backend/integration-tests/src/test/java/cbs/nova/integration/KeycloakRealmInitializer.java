1fe638d41d974dacffc53920821c53937cd2abd9
gleb619 <gleb86496@gmail.com>
2026-07-11 20:07:31 +0500

    feat(T52): add Keycloak realm initializer for integration tests

---
+++ backend/integration-tests/src/test/java/cbs/nova/integration/KeycloakRealmInitializer.java
@@ -0,0 +1,75 @@
+package cbs.nova.integration;
+
+import java.util.List;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.representations.idm.ClientRepresentation;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.RealmRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.testcontainers.containers.GenericContainer;
+
+public class KeycloakRealmInitializer {
+
+  private final String authServerUrl;
+  private final String realm = "cbs-nova";
+  private final String clientId = "cbs-nova-api";
+  private final String username = "test-user";
+  private final String password = "test-pass";
+
+  public KeycloakRealmInitializer(GenericContainer<?> keycloak) {
+    this.authServerUrl =
+        "http://" + keycloak.getHost() + ":" + keycloak.getMappedPort(8080);
+  }
+
+  public void initialize() {
+    try (Keycloak keycloak =
+        Keycloak.getInstance(authServerUrl, "master", "admin", "admin", "admin-cli")) {
+      RealmRepresentation realmRep = new RealmRepresentation();
+      realmRep.setRealm(realm);
+      realmRep.setEnabled(true);
+      keycloak.realms().create(realmRep);
+
+      var realmResource = keycloak.realm(realm);
+
+      ClientRepresentation client = new ClientRepresentation();
+      client.setClientId(clientId);
+      client.setEnabled(true);
+      client.setDirectAccessGrantsEnabled(true);
+      client.setPublicClient(false);
+      client.setSecret("test-secret");
+      realmResource.clients().create(client);
+
+      UserRepresentation user = new UserRepresentation();
+      user.setUsername(username);
+      user.setEnabled(true);
+
+      CredentialRepresentation credential = new CredentialRepresentation();
+      credential.setType(CredentialRepresentation.PASSWORD);
+      credential.setValue(password);
+      credential.setTemporary(false);
+      user.setCredentials(List.of(credential));
+
+      realmResource.users().create(user);
+    }
+  }
+
+  public String tokenEndpoint() {
+    return authServerUrl + "/realms/" + realm + "/protocol/openid-connect/token";
+  }
+
+  public String getClientId() {
+    return clientId;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public String getClientSecret() {
+    return "test-secret";
+  }
+}
