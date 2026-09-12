89c01aba5654265f87a6c9bb690debd2cc70c4c8
gleb619 <gleb86496@gmail.com>
2026-09-12 14:27:00 +0500

    feat(T470): extract docsBaseUrl git-remote parsing into tested util

--- frontend/admin-ui-plugin/app/layouts/admin.vue
+++ frontend/admin-ui-plugin/app/layouts/admin.vue
@@ -5,6 +5,7 @@ import {
   AppFooter,
   AppShell,
   AppSidebarToggle,
+  buildDocsBaseUrl,
   DslSavedDraftsWidget,
   useSavedDrafts,
 } from '@cbs/components'
@@ -65,25 +66,7 @@ const navItems = computed(() => [
   },
 ])
 
-const docsBaseUrl = computed(() => {
-  const git = info.value?.git
-  const origin = git?.remote?.origin?.url
-  if (!origin) return undefined
-
-  const branch = git?.branch ?? 'main'
-
-  const gitMatch = origin.match(/^git@github\.com:([^/]+)\/(.+?)(?:\.git)?$/)
-  if (gitMatch) {
-    return `https://github.com/${gitMatch[1]}/${gitMatch[2]}/blob/${branch}/docs/`
-  }
-
-  const httpsMatch = origin.match(/^https:\/\/github\.com\/([^/]+)\/(.+?)(?:\.git)?$/)
-  if (httpsMatch) {
-    return `https://github.com/${httpsMatch[1]}/${httpsMatch[2]}/blob/${branch}/docs/`
-  }
-
-  return undefined
-})
+const docsBaseUrl = computed(() => buildDocsBaseUrl(info.value?.git))
 
 const displayName = computed(
   () => user.value?.preferred_username ?? user.value?.name ?? user.value?.email ?? 'User',
